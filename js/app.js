70081616d5b743ca55302e3bed064ab493b5e3f3
Singa <labish.vova@gmail.com>
2021-09-19 20:49:00 +0300

    logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log(TEST);
+function insert(num) {
+    document.form.textView.value = 
+    document.form.textView.value + num;
+}
+
+function clean() {
+    document.form.textView.value = "";
+}
+
+function back() {
+    let point = document.form.textView.value;
+    document.form.textView.value = point.substring(0,point.length - 1);
+}
+
+function result() {
+    let point = document.form.textView.value;
+    if (point) {
+        document.form.textView.value = eval(point);
+    }
+}
+
+// console.log(document.form.textView);
